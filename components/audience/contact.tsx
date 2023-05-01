8c452b206e3a175ed9024f92d657df02363b44a5
SabeloX <sabelomtw@gmail.com>
2023-05-02 00:15:28 +0200

    Email service implemented

--- components/audience/contact.tsx
+++ components/audience/contact.tsx
@@ -1,8 +1,9 @@
-import { Box, Button, Container, FormControlLabel, FormGroup, IconButton, Radio, RadioGroup, TextField, Typography } from "@mui/material";
+import { Box, Button, Container, FormControlLabel, FormGroup, IconButton, Radio, RadioGroup, Snackbar, TextField, Typography } from "@mui/material";
 import { ChangeEvent, useState } from "react";
 import { ComponentShell } from "../component-shell";
 import PhoneIphoneIcon from '@mui/icons-material/PhoneIphone';
 import MailIcon from '@mui/icons-material/Mail';
+import axios from "axios";
 
 export interface ContactProps {
     textColor: string;
@@ -11,11 +12,20 @@ export interface ContactProps {
     email: string;
 }
 
+type StateVariable = {
+    value: string;
+    error: boolean;
+}
+
+//https://public.herotofu.com/v1/dcf54ab0-e864-11ed-b24a-93241516dd10 endpoint email service
+
 export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactProps) => {
-    const [name, setName] = useState<string>("");
-    const [emailAddress, setEmail] = useState<string>("");
-    const [message, setMessage] = useState<string>("");
-    const [subject, setSubject] = useState<string>("");
+    const [name, setName] = useState<StateVariable>({ value: "", error: false });
+    const [emailAddress, setEmail] = useState<StateVariable>({ value: "", error: false });
+    const [message, setMessage] = useState<StateVariable>({ value: "", error: false });
+    const [subject, setSubject] = useState<StateVariable>({ value: "", error: false });
+    const [open, setOpen] = useState<boolean>(false);
+    const [feeback, setFeedback] = useState<{ value: string; error: boolean }>({ value: "Email sent. I will chat with you soon!", error: false });
     return (
         <ComponentShell
             heading="Get in touch"
@@ -129,26 +139,34 @@ export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactP
                     variant="standard"
                     sx={{
                         color: `${textColor} !important`,
-                        borderBottom: `1px solid ${textColor} !important`,
+                        borderBottom: name.error ? "none" : `1px solid ${textColor}`,
                         "& .MuiFormLabel-root": {
                             color: `${textColor}`,
                         },
                         "& .MuiInputBase-input": {
                             color: `${textColor}`,
                         },
+                        "& .MuiFormControl-root": {
+                            borderBottom: "none",
+                        },
                         "& .MuiInputBase-root:after": {
                             borderBottom: `1px solid ${textColor} !important`,
-                        }
+                        },
+                        "& .MuiFormHelperText-root": {
+                            color: "red",
+                        },
                     }}
                     label="Name"
-                    value={name}
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => setName(event.target.value)}
+                    value={name.value}
+                    helperText={name.error && "Please enter your name"}
+                    error={name.error}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => setName({ value: event.target.value, error: false })}
                 />
                 <TextField
                     variant="standard"
                     sx={{
                         color: `${textColor} !important`,
-                        borderBottom: `1px solid ${textColor} !important`,
+                        borderBottom: emailAddress.error ? "none" : `1px solid ${textColor}`,
                         "& .MuiFormLabel-root": {
                             color: `${textColor}`,
                         },
@@ -157,12 +175,20 @@ export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactP
                         },
                         "& .MuiInputBase-root:after": {
                             borderBottom: `1px solid ${textColor} !important`,
+                        },
+                        "& .MuiFormControl-root": {
+                            border: "none",
+                        },
+                        "& .MuiFormHelperText-root": {
+                            color: "red",
                         }
                     }}
                     label="Email"
-                    value={emailAddress}
+                    value={emailAddress.value}
+                    helperText={emailAddress.error && "Please enter your email address"}
                     type="email"
-                    onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail(event.target.value)}
+                    error={emailAddress.error}
+                    onChange={(event: ChangeEvent<HTMLInputElement>) => setEmail({ value: event.target.value, error: false })}
                 />
                 <TextField
                     multiline
@@ -171,7 +197,7 @@ export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactP
                     label="Message"
                     sx={{
                         color: `${textColor} !important`,
-                        borderBottom: `1px solid ${textColor} !important`,
+                        borderBottom: message.error ? "none" : `1px solid ${textColor}`,
                         "& .MuiFormLabel-root": {
                             color: `${textColor}`,
                         },
@@ -180,10 +206,18 @@ export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactP
                         },
                         "& .MuiInputBase-root:after": {
                             borderBottom: `1px solid ${textColor} !important`,
+                        },
+                        "& .MuiFormControl-root": {
+                            border: "none",
+                        },
+                        "& .MuiFormHelperText-root": {
+                            color: "red",
                         }
                     }}
-                    value={message}
-                    onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setMessage(event.target.value)}
+                    error={message.error}
+                    helperText={message.error && "Please enter your message"}
+                    value={message.value}
+                    onChange={(event: ChangeEvent<HTMLTextAreaElement>) => setMessage({ value: event.target.value, error: false })}
                 />
                 <FormGroup>
                     <RadioGroup
@@ -196,7 +230,7 @@ export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactP
                                 color: textColor,
                             }
                         }}
-                        onChange={(event, value) => setSubject(value)}
+                        onChange={(event, value) => setSubject({ value, error: false })}
                     >
                         {
                             ["Request a service", "Enquire about me", "Just saying hi!"].map((item, index) => (
@@ -210,6 +244,17 @@ export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactP
                             ))
                         }
                     </RadioGroup>
+                    {subject.error &&
+                        <Typography
+                            sx={{
+                                color: "red",
+                                fontSize: "12px"
+                            }}
+                            className="text"
+                        >
+                            Please choose the reason.
+                        </Typography>
+                    }
                 </FormGroup>
                 <Button
                     sx={{
@@ -222,10 +267,76 @@ export const Contact = ({ textColor, lightShadeColor, contact, email }: ContactP
                         textTransform: "unset !important",
                         padding: "10px 0"
                     }}
+                    onClick={() => {
+                        if (name.value === "" || emailAddress.value === "" || subject.value === "" || message.value === "") {
+                            if (name.value === "") {
+                                setName({ value: name.value, error: true })
+                            }
+                            if (emailAddress.value === "") {
+                                setEmail({ value: emailAddress.value, error: true })
+                            }
+                            if (subject.value === "") {
+                                setSubject({ value: subject.value, error: true })
+                            }
+                            if (message.value === "") {
+                                setMessage({ value: message.value, error: true })
+                            }
+                        }
+                        else {
+                            axios.post(
+                                "https://public.herotofu.com/v1/dcf54ab0-e864-11ed-b24a-93241516dd10",
+                                {
+                                    name: name.value,
+                                    email: emailAddress.value,
+                                    subject: subject.value,
+                                    message: message.value
+                                }).
+                                then(result => {
+                                    setName({ value: "", error: false });
+                                    setEmail({ value: "", error: false });
+                                    setMessage({ value: "", error: false });
+                                    setSubject({ value: "", error: false });
+                                    setOpen(true);
+                                })
+                                .catch(error => {
+                                    setFeedback({ value: "An error has occured. Please contact the email: sabelo.x.mtetwa@gmail.com", error: true });
+                                    setOpen(true);
+                                })
+                        }
+                    }}
                 >
                     Send
                 </Button>
+                <div
+                    style={{
+                        textIndent: "-99999px",
+                        whiteSpace: "nowrap",
+                        overflow: "hidden",
+                        position: "absolute"
+                    }}
+                    aria-hidden="true"
+                >
+                    <input type="text" name="_gotcha" tabIndex={-1} autoComplete="off" />
+                </div>
             </Container>
+            <Snackbar
+                open={open}
+                sx={{
+                    backgroundColor: lightShadeColor,
+                    padding: "20px",
+                    borderRadius: "20px",
+                }}
+                autoHideDuration={500}
+            >
+                <Typography
+                    sx={{
+                        color: feeback.error ? "red" : "green"
+                    }}
+                    className="text"
+                >
+                    {feeback.value}
+                </Typography>
+            </Snackbar>
         </ComponentShell>
     )
 }
